2aae5ca908cdd2d3eeab652b6728c6021f35a9c1
Jett Thompson <jett.thompson.59@gmail.com>
2021-10-17 14:55:07 +1100

    Added a usage message if not enough arguments are passed

--- Delta-test/Delta-test.cpp
+++ Delta-test/Delta-test.cpp
@@ -1,10 +1,17 @@
 ﻿#include <iostream>
+#include <string>
 
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::string;
 
-int main()
-{
-	cout << "Hello there" << endl;
-	return 0;
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    cerr << "Usage: " << argv[0] << " <encode | decode> <File>" << endl;
+    cerr << "Encodes or decodes a delta encoded file" << endl;
+    return 0;
+  }
+  string command = string(argv[1]);
+  return 0;
 }
